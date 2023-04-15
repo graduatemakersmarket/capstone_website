97c249b503c3951ea1f4165c20d0433ee1667112
Michael Umland <michael@umland.dev>
2023-04-14 21:11:12 -0500

    Updated external dependencies and removed bloated components we didn't need

---
+++ static/js/animations.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+	// AOS rendering animations
+	if (typeof(AOS) !== "undefined"){
+		// Animation Settings
+		AOS.init({
+			easing: 'ease-out-cubic',
+			offset: 50
+		})
+	}
+})
