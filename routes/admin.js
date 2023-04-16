b07d55993dd25b8e9e033d4624ec3d45fe7dc7ef
Michael Umland <michael@umland.dev>
2023-04-15 23:55:43 -0500

    Fixed the admin page

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router()
 router.get("/", (request, response) => {
     
     //I'm not entirely sure how to pass in objects for the user's profile but here it is
-    const profile = "User profile"
+    const profile = ["user1", "user2", "user3"]
 
     return response
     .status(200)
