2dd389670a46ed4db3c6eb61094e484790d40552
Michael <mhumland@uark.edu>
2023-09-14 03:53:36 -0500

    Fixed a bug introduced from the merge

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -4,7 +4,7 @@ const roleService = require('../services/roleService');
 /* Helper Methods
 /*************************************************************************************************/
 const createRole = async (role) => roleService.createRole(role);
-const getRolesByEmail = async (account_email) => (await roleService.getRoles(account_email)).map((role) => role.role);
+const getRolesByEmail = async (account_email) => (await roleService.getRolesByEmail(account_email)).map((role) => role.role);
 
 module.exports = {
     createRole,
